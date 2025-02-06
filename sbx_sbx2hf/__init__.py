b558e2a2125f4b17e9ced3193f844b64bfe451b4
Felix <felixhultin@yahoo.se>
2025-02-06 16:32:27 +0100

    first working version with sbx2hf

--- sbx_sbx2hf/__init__.py
+++ sbx_sbx2hf/__init__.py
@@ -3,7 +3,6 @@ from sparv.api import Config
 from . import exporters
 
 __config__ = [
-    Config("sbx_sbx2hf.config", None, "Configuration variables for sbx2hf"),
 ]
 
 __description__ = "A Sparv plugin for uploading export files to HuggingFace."
